3eb5482479fbcd081b48e3f8e883ca4288234440
Limber Jhonathan <limberjhonathan@gmail.com>
2024-09-11 22:24:51 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,18 @@
 import { FastifyInstance, FastifyRequest, FastifyReply, FastifyPluginOptions } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomersController";
+import { ListCustomersController } from "./controllers/ListCustomersController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+
     fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
         return { message: "Rota de teste funcionando!" };
+    });
+
+    fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply);
+    });
+
+    fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListCustomersController().handle(request, reply);
     });
 }
