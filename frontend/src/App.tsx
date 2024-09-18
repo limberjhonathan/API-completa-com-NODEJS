c497b100cc2b5cf393b0181394eed929f4cc9c5b
Limber Jhonathan <limberjhonathan@gmail.com>
2024-09-17 23:23:39 -0300

    frontend nome + email

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default function App(){
+  return(
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
-function App() {
-  const [count, setCount] = useState(0)
+        <form className="flex flex-col my-6">
+          <label className="font-medium text-white">Nome: </label>
+          <input 
+            type="text"
+            placeholder="Digite seu nome completo...."
+            className="w-full mb-5 p-2 rounded"
+          />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <label className="font-medium text-white">Nome: </label>
+          <input 
+            type="email"
+            placeholder="Digite seu email...."
+            className="w-full mb-5 p-2 rounded"
+          />
+        </form>
+      </main>
+    </div>
   )
 }
-
-export default App
